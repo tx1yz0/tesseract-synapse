d29723019f789f7e0e479ddfd943a9a69a4adf51
Tx1 <tx1yz0@gmail.com>
2024-03-08 17:37:40 +0530

    first commit

---
+++ mocks/faq.tsx
@@ -0,0 +1,44 @@
+export const faq = [
+    {
+        id: "0",
+        title: "What is Tesseract?",
+        content:
+            "Tesseract is a messaging platform that uses artificial intelligence to communicate with users. It can understand natural language and provide helpful responses to your questions and requests.",
+    },
+    {
+        id: "1",
+        title: "How does Tesseract work?",
+        content:
+            "You can ask Tesseract anything you would ask a human assistant, such as weather updates, news headlines, restaurant recommendations, and more.",
+    },
+    {
+        id: "2",
+        title: "What kind of questions can I ask Tesseract?",
+        content:
+            "Tesseract is a messaging platform that uses artificial intelligence to communicate with users. It can understand natural language and provide helpful responses to your questions and requests.",
+    },
+    {
+        id: "3",
+        title: "Is my data safe with Tesseract?",
+        content:
+            "You can ask Tesseract anything you would ask a human assistant, such as weather updates, news headlines, restaurant recommendations, and more.",
+    },
+    {
+        id: "4",
+        title: "Can I customize Tesseract's responses?",
+        content:
+            "Tesseract is a messaging platform that uses artificial intelligence to communicate with users. It can understand natural language and provide helpful responses to your questions and requests.",
+    },
+    {
+        id: "5",
+        title: "Is Tesseract available in multiple languages?",
+        content:
+            "You can ask Tesseract anything you would ask a human assistant, such as weather updates, news headlines, restaurant recommendations, and more.",
+    },
+    {
+        id: "6",
+        title: "Can I use Tesseract for business purposes?",
+        content:
+            "Tesseract is a messaging platform that uses artificial intelligence to communicate with users. It can understand natural language and provide helpful responses to your questions and requests.",
+    },
+];
